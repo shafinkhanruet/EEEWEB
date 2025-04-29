3ee28e992c8c9d6d07303aa03dc5af472a2c8c0f
wasi20280 <wasi20280@gmail.com>
2025-04-29 17:22:45 +0600

    Drive

--- src/data/resources.js
+++ src/data/resources.js
@@ -4,14 +4,14 @@ export const resources = [
     title: '1-1(Firoz 2201075)',
     description: 'Complete collection of notes, assignments, and past question papers for 1-1 term including EEE, CSE, Math, Physics, and English materials.',
     fileType: 'Multiple files',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id-1'
+    driveUrl: 'https://drive.google.com/drive/folders/1EU3B6I-VUCbLunJpch5FlqhrDyiFpaKM?fbclid=IwY2xjawGMndtleHRuA2FlbQIxMAABHZa1ImA77ZrHSb_RLYstFj8hN88oso5qhV__L4Wmzd5t2qXoHt72O7-raA_aem_eyvGkCpWig83vZa3celANw'
   },
   {
     id: '2',
     title: 'SIFAT EEE-A 2301002',
     description: 'Comprehensive physics resources including Atomic Structure, Diffraction, Electronic Structure of Matter, Interference, Magnetism, and more.',
     fileType: 'Multiple PDF files',
-    driveUrl: 'https://drive.google.com/drive/folders/your-folder-id-2'
+    driveUrl: 'https://drive.google.com/drive/u/1/folders/1inaXCLgG1NGOFzu4h2yUz0_mszfd-Mf-'
   }
 ];
 
